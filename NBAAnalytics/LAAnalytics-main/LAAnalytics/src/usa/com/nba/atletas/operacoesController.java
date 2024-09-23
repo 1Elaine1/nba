e75f10acf5aadc95ff50a31877faf71fed03e2c6
1Elaine1 <elannenanda025@gmail.com>
2024-09-23 20:57:50 -0300

    Add files via upload

---
+++ NBAAnalytics/LAAnalytics-main/LAAnalytics/src/usa/com/nba/atletas/operacoesController.java
@@ -0,0 +1,26 @@
+package usa.com.nba.atletas;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class operacoesController {
+	@FXML
+	private TextField txfnome;
+	
+	@FXML
+	private TextField txfsobrenome;
+	
+	@FXML
+	private TextField txfidade;
+	
+	@FXML
+	private TextField txfposicao;
+	
+	@FXML
+	private TextField txfequipe;
+	
+	@FXML
+	private Button btncadastrar;
+	
+}
